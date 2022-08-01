5436984034319065c42188980be557034f1f4471
Lucas Ebbinghaus <lucasebbinghaus2016@gmail.com>
2022-08-01 14:38:09 -0400

    make four div boxes and be able to change color when clicked

---
+++ etch.js
@@ -0,0 +1,13 @@
+const hover1=document.querySelector(".square");
+hover1.addEventListener("click", changeColor);
+
+
+
+function changeColor()
+{
+
+    hover1.style.backgroundColor = "black";
+
+
+
+}

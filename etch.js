b196b3b1ed00a9e6d697d537e29e78bbce02aa9d
Lucas Ebbinghaus <lucasebbinghaus2016@gmail.com>
2022-08-10 19:42:07 -0400

    change exclamation point number and box color to aqua

--- etch.js
+++ etch.js
@@ -2,12 +2,22 @@ const hover1=document.querySelector(".square");
 hover1.addEventListener("click", changeColor);
 
 
+const arr2 = new Array(2);
+
+for (let i = 0; i < arr2.length; i++) {
+  arr2[i] = {key: 'value'};
+  alert(arr2[i]);
+}
+
+// 👇️ [{key: 'value'}, {key: 'value'}]
+
+
+
+
+
+
 
 function changeColor()
 {
-
     hover1.style.backgroundColor = "black";
-
-
-
 }

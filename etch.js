8bc50ecd7ab1b1960c65f545703b6b46b386d624
Lucas Ebbinghaus <lucasebbinghaus2016@gmail.com>
2022-08-15 16:22:18 -0400

    get rid of manual div box in html and add by javascript. Fix ability to select all boxes and change their color.

--- etch.js
+++ etch.js
@@ -1,23 +1,27 @@
-const hover1=document.querySelector(".square");
-hover1.addEventListener("click", changeColor);
-
-
-const arr2 = new Array(2);
-
-for (let i = 0; i < arr2.length; i++) {
-  arr2[i] = {key: 'value'};
-  alert(arr2[i]);
-}
-
-// 👇️ [{key: 'value'}, {key: 'value'}]
 
 
 
+   const array0=new Array(5);
+    for(i=0;i<array0.length;i++)                    //creates array of 5 boxes
+    {
+    const array1=document.createElement('div');
+    array1.className=("square");
+    container.appendChild(array1);
+    
+    }
+    
+    
 
 
+    
+    
 
+const hover1=document.querySelectorAll(".square").forEach(hover1=>{      //click on each box and they
+    hover1.addEventListener("click",changeColor =>{                      // change color
+        hover1.style.backgroundColor="black";
+    })
+})
 
-function changeColor()
-{
-    hover1.style.backgroundColor = "black";
-}
+   
+   
+  
